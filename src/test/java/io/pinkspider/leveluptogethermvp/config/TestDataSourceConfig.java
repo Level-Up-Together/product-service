622f3b22410fc6999e97673874bc1f25eed88614
Yungoo Park <ygpark@lendingmachine.co.kr>
2025-12-09 15:25:48 +0900

    fix: 테스트 환경 DataSource 설정을 프로덕션과 동일하게 분리
      기존에는 테스트 환경에서 단일 EntityManagerFactory가 모든 서비스 패키지를
      스캔하여 userDataSource만 사용했기 때문에 mission, guild 등 모든 엔티티가
      user_db에 생성되는 문제가 있었음.
    
      변경 사항:
      - TestDataSourceConfig를 서비스별 독립 설정으로 분리
        - TestUserDataSourceConfig (userservice -> user_db)
        - TestMissionDataSourceConfig (missionservice -> mission_db)
        - TestGuildDataSourceConfig (guildservice -> guild_db)
        - TestMetaDataSourceConfig (metaservice -> meta_db)
        - TestSagaDataSourceConfig (saga.persistence -> saga_db)
      - 각 서비스별 EntityManagerFactory, TransactionManager 분리 생성
      - application-test.yml에서 default_catalog: user_db 설정 제거

--- src/test/java/io/pinkspider/leveluptogethermvp/config/TestDataSourceConfig.java
+++ src/test/java/io/pinkspider/leveluptogethermvp/config/TestDataSourceConfig.java
@@ -1,78 +1,261 @@
 package io.pinkspider.leveluptogethermvp.config;
 
-import java.util.HashMap;
-import java.util.Map;
+import jakarta.persistence.EntityManagerFactory;
+import java.util.Properties;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @Profile("test")
-@EnableJpaRepositories(
-    basePackages = {
-        "io.pinkspider.leveluptogethermvp",
-        "io.pinkspider.global.saga.persistence"
-    }
-)
+@EnableTransactionManagement
 public class TestDataSourceConfig {
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.datasource.user")
-    public DataSource userDataSource() {
-        return DataSourceBuilder.create().build();
+    private Properties jpaProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        properties.setProperty("hibernate.format_sql", "true");
+        properties.setProperty("hibernate.show_sql", "true");
+        return properties;
     }
 
-    @Bean
-    @ConfigurationProperties("spring.datasource.mission")
-    public DataSource missionDataSource() {
-        return DataSourceBuilder.create().build();
+    // ========== User DataSource ==========
+    @Configuration
+    @Profile("test")
+    @EnableJpaRepositories(
+        basePackages = "io.pinkspider.leveluptogethermvp.userservice",
+        entityManagerFactoryRef = "userEntityManagerFactory",
+        transactionManagerRef = "userTransactionManager"
+    )
+    static class TestUserDataSourceConfig {
+
+        @Bean
+        @Primary
+        @ConfigurationProperties("spring.datasource.user")
+        public DataSource userDataSource() {
+            return DataSourceBuilder.create().build();
+        }
+
+        @Bean(name = "userEntityManagerFactory")
+        @Primary
+        public LocalContainerEntityManagerFactoryBean userEntityManagerFactory(
+            @Qualifier("userDataSource") DataSource dataSource) {
+            LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+            em.setDataSource(dataSource);
+            em.setPackagesToScan("io.pinkspider.leveluptogethermvp.userservice");
+            em.setPersistenceUnitName("user");
+            HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+            em.setJpaVendorAdapter(vendorAdapter);
+            em.setJpaProperties(jpaProperties());
+            return em;
+        }
+
+        @Bean(name = "userTransactionManager")
+        @Primary
+        public PlatformTransactionManager userTransactionManager(
+            @Qualifier("userEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+            return new JpaTransactionManager(entityManagerFactory);
+        }
+
+        private Properties jpaProperties() {
+            Properties properties = new Properties();
+            properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+            properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+            properties.setProperty("hibernate.format_sql", "true");
+            properties.setProperty("hibernate.show_sql", "true");
+            return properties;
+        }
     }
 
-    @Bean
-    @ConfigurationProperties("spring.datasource.guild")
-    public DataSource guildDataSource() {
-        return DataSourceBuilder.create().build();
+    // ========== Mission DataSource ==========
+    @Configuration
+    @Profile("test")
+    @EnableJpaRepositories(
+        basePackages = "io.pinkspider.leveluptogethermvp.missionservice",
+        entityManagerFactoryRef = "missionEntityManagerFactory",
+        transactionManagerRef = "missionTransactionManager"
+    )
+    static class TestMissionDataSourceConfig {
+
+        @Bean
+        @ConfigurationProperties("spring.datasource.mission")
+        public DataSource missionDataSource() {
+            return DataSourceBuilder.create().build();
+        }
+
+        @Bean(name = "missionEntityManagerFactory")
+        public LocalContainerEntityManagerFactoryBean missionEntityManagerFactory(
+            @Qualifier("missionDataSource") DataSource dataSource) {
+            LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+            em.setDataSource(dataSource);
+            em.setPackagesToScan("io.pinkspider.leveluptogethermvp.missionservice");
+            em.setPersistenceUnitName("mission");
+            HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+            em.setJpaVendorAdapter(vendorAdapter);
+            em.setJpaProperties(jpaProperties());
+            return em;
+        }
+
+        @Bean(name = "missionTransactionManager")
+        public PlatformTransactionManager missionTransactionManager(
+            @Qualifier("missionEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+            return new JpaTransactionManager(entityManagerFactory);
+        }
+
+        private Properties jpaProperties() {
+            Properties properties = new Properties();
+            properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+            properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+            properties.setProperty("hibernate.format_sql", "true");
+            properties.setProperty("hibernate.show_sql", "true");
+            return properties;
+        }
     }
 
-    @Bean
-    @ConfigurationProperties("spring.datasource.meta")
-    public DataSource metaDataSource() {
-        return DataSourceBuilder.create().build();
+    // ========== Guild DataSource ==========
+    @Configuration
+    @Profile("test")
+    @EnableJpaRepositories(
+        basePackages = "io.pinkspider.leveluptogethermvp.guildservice",
+        entityManagerFactoryRef = "guildEntityManagerFactory",
+        transactionManagerRef = "guildTransactionManager"
+    )
+    static class TestGuildDataSourceConfig {
+
+        @Bean
+        @ConfigurationProperties("spring.datasource.guild")
+        public DataSource guildDataSource() {
+            return DataSourceBuilder.create().build();
+        }
+
+        @Bean(name = "guildEntityManagerFactory")
+        public LocalContainerEntityManagerFactoryBean guildEntityManagerFactory(
+            @Qualifier("guildDataSource") DataSource dataSource) {
+            LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+            em.setDataSource(dataSource);
+            em.setPackagesToScan("io.pinkspider.leveluptogethermvp.guildservice");
+            em.setPersistenceUnitName("guild");
+            HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+            em.setJpaVendorAdapter(vendorAdapter);
+            em.setJpaProperties(jpaProperties());
+            return em;
+        }
+
+        @Bean(name = "guildTransactionManager")
+        public PlatformTransactionManager guildTransactionManager(
+            @Qualifier("guildEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+            return new JpaTransactionManager(entityManagerFactory);
+        }
+
+        private Properties jpaProperties() {
+            Properties properties = new Properties();
+            properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+            properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+            properties.setProperty("hibernate.format_sql", "true");
+            properties.setProperty("hibernate.show_sql", "true");
+            return properties;
+        }
     }
 
-    @Bean
-    @ConfigurationProperties("spring.datasource.saga")
-    public DataSource sagaDataSource() {
-        return DataSourceBuilder.create().build();
+    // ========== Meta DataSource ==========
+    @Configuration
+    @Profile("test")
+    @EnableJpaRepositories(
+        basePackages = "io.pinkspider.leveluptogethermvp.metaservice",
+        entityManagerFactoryRef = "metaEntityManagerFactory",
+        transactionManagerRef = "metaTransactionManager"
+    )
+    static class TestMetaDataSourceConfig {
+
+        @Bean
+        @ConfigurationProperties("spring.datasource.meta")
+        public DataSource metaDataSource() {
+            return DataSourceBuilder.create().build();
+        }
+
+        @Bean(name = "metaEntityManagerFactory")
+        public LocalContainerEntityManagerFactoryBean metaEntityManagerFactory(
+            @Qualifier("metaDataSource") DataSource dataSource) {
+            LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+            em.setDataSource(dataSource);
+            em.setPackagesToScan("io.pinkspider.leveluptogethermvp.metaservice");
+            em.setPersistenceUnitName("meta");
+            HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+            em.setJpaVendorAdapter(vendorAdapter);
+            em.setJpaProperties(jpaProperties());
+            return em;
+        }
+
+        @Bean(name = "metaTransactionManager")
+        public PlatformTransactionManager metaTransactionManager(
+            @Qualifier("metaEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+            return new JpaTransactionManager(entityManagerFactory);
+        }
+
+        private Properties jpaProperties() {
+            Properties properties = new Properties();
+            properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+            properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+            properties.setProperty("hibernate.format_sql", "true");
+            properties.setProperty("hibernate.show_sql", "true");
+            return properties;
+        }
     }
 
-    @Bean
-    @Primary
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-        @Qualifier("userDataSource") DataSource dataSource,
-        EntityManagerFactoryBuilder builder) {
+    // ========== Saga DataSource ==========
+    @Configuration
+    @Profile("test")
+    @EnableJpaRepositories(
+        basePackages = "io.pinkspider.global.saga.persistence",
+        entityManagerFactoryRef = "sagaEntityManagerFactory",
+        transactionManagerRef = "sagaTransactionManager"
+    )
+    static class TestSagaDataSourceConfig {
 
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto", "create-drop");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        @Bean
+        @ConfigurationProperties("spring.datasource.saga")
+        public DataSource sagaDataSource() {
+            return DataSourceBuilder.create().build();
+        }
 
-        return builder
-            .dataSource(dataSource)
-            .packages(
-                "io.pinkspider.leveluptogethermvp",
-                "io.pinkspider.global.saga.persistence"
-            )
-            .persistenceUnit("test")
-            .properties(properties)
-            .build();
+        @Bean(name = "sagaEntityManagerFactory")
+        public LocalContainerEntityManagerFactoryBean sagaEntityManagerFactory(
+            @Qualifier("sagaDataSource") DataSource dataSource) {
+            LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+            em.setDataSource(dataSource);
+            em.setPackagesToScan("io.pinkspider.global.saga.persistence");
+            em.setPersistenceUnitName("saga");
+            HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+            em.setJpaVendorAdapter(vendorAdapter);
+            em.setJpaProperties(jpaProperties());
+            return em;
+        }
+
+        @Bean(name = "sagaTransactionManager")
+        public PlatformTransactionManager sagaTransactionManager(
+            @Qualifier("sagaEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+            return new JpaTransactionManager(entityManagerFactory);
+        }
+
+        private Properties jpaProperties() {
+            Properties properties = new Properties();
+            properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+            properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+            properties.setProperty("hibernate.format_sql", "true");
+            properties.setProperty("hibernate.show_sql", "true");
+            return properties;
+        }
     }
 }
